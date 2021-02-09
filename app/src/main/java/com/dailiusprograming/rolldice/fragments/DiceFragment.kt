1e703c202472a8ad9e5622c303f9b85c07081db8
Dailius <dailius.rascius@gmail.com>
2021-02-09 18:51:25 +0200

    New DiceHelper in util package 	- rollDice() returns random results of 5 dice 	- calculateDice() calculate dice result of selected dice number 	- evaluateDice () evaluates combination results like "full house", "line" etc. New strings with 5 dice combination names DiceFragment.kt new bind button with ClickListener and toast message

--- app/src/main/java/com/dailiusprograming/rolldice/fragments/DiceFragment.kt
+++ app/src/main/java/com/dailiusprograming/rolldice/fragments/DiceFragment.kt
@@ -1,32 +1,58 @@
 package com.dailiusprograming.rolldice.fragments
 
+import android.content.Context
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.dailiusprograming.rolldice.R
+import com.dailiusprograming.rolldice.databinding.DiceFragmentBinding
+import com.dailiusprograming.rolldice.util.DiceHelper
 
 class DiceFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = DiceFragment()
-    }
+//    companion object {
+//        fun newInstance() = DiceFragment()
+//    }
 
     private lateinit var viewModel: DiceViewModel
+    private var diceFragmentBinding: DiceFragmentBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.dice_fragment, container, false)
+        val binding = DiceFragmentBinding.inflate(inflater, container, false)
+        diceFragmentBinding = binding
+
+        binding.btnRoll.setOnClickListener {
+            val rollDice = DiceHelper.rollDice()
+            val data = DiceHelper.calculateDice(3, rollDice)
+            Toast.makeText(
+                context,
+                "rolled dice ${rollDice.size}, $data, ${rollDice[0]} ",
+                Toast.LENGTH_SHORT
+            ).show()
+
+        }
+
+
+
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(DiceViewModel::class.java)
         // TODO: Use the ViewModel
+    }
+
+    override fun onDestroyView() {
+        diceFragmentBinding = null
+        super.onDestroyView()
     }
 
 }
