19b6b4ade1ba19c773d83ed66b2816e0a150ca5b
Dailius <dailius.rascius@gmail.com>
2021-03-18 10:59:45 +0200

    Code reformat ...

--- app/src/main/java/com/dailiusprograming/rolldice/Constants.kt
+++ app/src/main/java/com/dailiusprograming/rolldice/Constants.kt
@@ -2,9 +2,9 @@ package com.dailiusprograming.rolldice
 
 const val PREF_NAME = "Settings"
 const val DICE_COLOR_ID = 0
-const val ANIM_DICE_START:Long = 300
-const val ANIM_DICE_END:Long = 700
-const val ANIM_TEXT_START:Long = 100
-const val ANIM_IBTN:Long = 1000
+const val ANIM_DICE_START: Long = 300
+const val ANIM_DICE_END: Long = 700
+const val ANIM_TEXT_START: Long = 100
+const val ANIM_IBTN: Long = 1000
 
 
